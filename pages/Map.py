c66efbbfe07b9cf7e0b2f7fe8370ceab77e443aa
Ross Stuart <Ross.Stuart@ahead.com>
2022-07-21 16:12:58 -0500

    update Map to add tooltip

--- pages/Map.py
+++ pages/Map.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 import urllib
 import pandas as pd
+import pydeck as pdk
 
 st.set_page_config(
         page_icon="🚲", 
@@ -31,7 +32,7 @@ data = response.read()
 values = json.loads(data)
 stations_info = values["data"]["stations"]
 
-station_points = pd.DataFrame(columns = ['lat', 'lon'])
+station_points = pd.DataFrame(columns = ['lat', 'lon','legacy_id'])
 station_list = ["185", "222", "47", "196", "116", "316", "285", "125"]
 for station in stations_info:
     if station['legacy_id'] in station_list:
@@ -39,8 +40,38 @@ for station in stations_info:
             station['legacy_id'],": ", station['name']
             )
         # add point to df
-        station_points.loc[len(station_points)] = [station['lat'], station['lon']]
+        station_points.loc[len(station_points)] = [station['lat'], station['lon'], station['legacy_id']]
 
 # Map the points
 # st.write(station_points)
-st.map(station_points)
+# st.map(station_points)
+# st.pydeck_chart(station_points)
+
+view = pdk.ViewState(latitude=41.8781, longitude=-87.62987, zoom=11,)
+# Create the scatter plot layer
+StationLayer = pdk.Layer(
+        "ScatterplotLayer",
+        data=station_points,
+        pickable=True,
+        opacity=0.3,
+        stroked=True,
+        filled=True,
+        radius_scale=10,
+        radius_min_pixels=5,
+        radius_max_pixels=60,
+        line_width_min_pixels=1,
+        get_position=["lon", "lat"],
+        # get_radius=metric_to_show_in_covid_Layer,
+        get_fill_color=[252, 136, 3],
+        get_line_color=[255,0,0],
+        # tooltip={"text": "{legacy_id}"},
+    )
+
+r = pdk.Deck(
+    layers=[StationLayer],
+    initial_view_state=view,
+    map_style="mapbox://styles/mapbox/light-v10",
+    tooltip={"text": "{legacy_id}"},
+)
+
+map = st.pydeck_chart(r)
