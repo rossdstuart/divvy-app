65f4fa68dce4b0a026faf83efc7e5fcc33674183
Ross Stuart <Ross.Stuart@ahead.com>
2022-08-09 05:28:37 -0500

    commenting out ttl for testing

--- Home.py
+++ Home.py
@@ -36,7 +36,8 @@ conn = init_connection()
 
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
+# Testing to remove the ttl to see if it saves on query costs
+# @st.experimental_memo(ttl=600) 
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
